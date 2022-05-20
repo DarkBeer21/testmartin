021e852ecc57b0f80949068bc5fe883fa5913a1e
José <Jos@Darkcito>
2022-05-20 15:06:07 -0300

    Mod App.java

--- Practica/src/main/java/com/cala/App.java
+++ Practica/src/main/java/com/cala/App.java
@@ -11,6 +11,6 @@ public class App
     public static void main( String[] args )
     {
         //System.out.println( "Hello World!" );
-        JOptionPane.showMessageDialog(null,"Hola");
+        JOptionPane.showMessageDialog(null,"Hola Mundo");
     }
 }
